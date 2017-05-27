97e4e326156280d061efcda3e8455f8df5a926e3
glnosg <glina.osg@gmail.com>
2017-05-27 17:18:17 +0200

    Add try/catch to play method and compile MusicTest1 class

--- MusicTest1.java
+++ MusicTest1.java
@@ -3,9 +3,13 @@ import javax.sound.midi.*;
 public class MusicTest1 {
 	
 	public void play() {
-		Sequencer sequencer = MidiSystem.getSequencer();
 		
-		System.out.println("We got sequencer");
+		try {
+			Sequencer sequencer = MidiSystem.getSequencer();
+			System.out.println("We got sequencer");
+		} catch(MidiUnavailableException ex) {
+			System.out.println("Bummer");
+		}
 	}
 	
 	public static void main(String[] args) {
